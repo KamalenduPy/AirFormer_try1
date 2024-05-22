c0cfa2d02674d7c07361364f66901e765b08f721
KamalenduPy <41977062+KamalenduPy@users.noreply.github.com>
2024-05-22 11:12:36 +0530

    Update embedding.py
    added season embedding

--- src/layers/embedding.py
+++ src/layers/embedding.py
@@ -1,21 +1,23 @@
 import torch
 import torch.nn as nn
 
-class AirEmbedding(nn.Module):
+class SeasonEmbedding(nn.Module):
     '''
     Embed catagorical variables.
     '''
     def __init__(self):
         super(AirEmbedding, self).__init__()
-        self.embed_wdir=nn.Embedding(11,3)
-        self.embed_weather=nn.Embedding(18,4)
-        self.embed_day=nn.Embedding(24,3) # a typo here but doesn't affect the results. this layer is actually for embedding 24 hours.
-        self.embed_hour=nn.Embedding(7,5) # a typo here but doesn't affect the results. this layer is actually for embedding 7 days.
+        self.embed_hour=nn.Embedding(26,5)
+        self.embed_week=nn.Embedding(11,3)
+        self.embed_month=nn.Embedding(18,3)
+        self.embed_dom=nn.Embedding(32,3) # a typo here but doesn't affect the results. this layer is actually for embedding 24 hours.
+        self.embed_season=nn.Embedding(7,5) # a typo here but doesn't affect the results. this layer is actually for embedding 7 days.
 
     def forward(self, x):
-        x_wdir = self.embed_wdir(x[...,0])
-        x_weather = self.embed_weather(x[...,1])
-        x_day = self.embed_day(x[...,2])
-        x_hour = self.embed_hour(x[...,3])
-        out=torch.cat((x_wdir,x_weather,x_day,x_hour),-1)
+        x_hour = self.embed_hour(x[...,0])
+        x_week = self.embed_week(x[...,1])
+        x_month = self.embed_month(x[...,2])
+        x_dom = self.embed_dom(x[...,3])
+        x_season = self.embed_season(x[...,4])
+        out=torch.cat((x_hour,x_week,x_month,x_dom,x_season),-1)
         return out
